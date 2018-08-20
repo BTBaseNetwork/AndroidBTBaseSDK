e71f2114c8781618eb6c7c759a0d7080d8a860ac
alexchow <cplover@live.com>
2018-08-20 16:23:30 +0800

    update

---
+++ btbasesdk/src/main/java/mobi/btbase/btbasesdk/FullscreenVideoActivity.java
@@ -0,0 +1,13 @@
+package mobi.btbase.btbasesdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullscreenVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen_video);
+    }
+}
